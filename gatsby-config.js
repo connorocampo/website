195c471b5ebcf9e874dc8ee7275640fed30f5761
Connor Ocampo <ConnorOcampo@gmail.com>
2019-05-27 17:15:09 -0400

    Add gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,29 @@
 module.exports = {
+  siteMetadata: {
+    title: `Connor Ocampo's Website`,
+    author: `Connor Ocampo`,
+    description: `Connor Ocampo's Website.`,
+    siteUrl: `https://www.connorocampo.com/`,
+  },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Connor Ocampo`,
+        short_name: `Connor`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#345d7e`,
+        display: `standalone`,
+      },
+    },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      }
+    },
   ]
 }

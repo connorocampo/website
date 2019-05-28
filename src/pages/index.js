b9b7579a5cc62e27a05b4b5562641664d3b7e252
Connor Ocampo <ConnorOcampo@gmail.com>
2019-05-28 13:56:19 -0400

    Reorder imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import { Helmet } from 'react-helmet';
 // import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
-
 import { graphql } from "gatsby";
 
 import themeSettings from '../components/base/settings';
+
+// Stylesheets
+
+import "../assets/styles/main.css";
+import "../assets/styles/header.css";
+import "../assets/styles/footer.css";
 
 // Components
 
@@ -14,11 +19,6 @@ import Bio from "../components/home/Bio";
 import Writings from "../components/home/Writings";
 import Footer from "../components/base/Footer";
 
-// Stylesheets
-
-import "../assets/styles/main.css";
-import "../assets/styles/header.css";
-import "../assets/styles/footer.css";
 
 // const Container = styled.div`
 //   max-width: 1000px;

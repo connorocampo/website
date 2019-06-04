f096a7023139c6da881aa15c5049b7eaf931c5ad
Connor Ocampo <ConnorOcampo@gmail.com>
2019-06-04 11:45:27 -0400

    Add form styles

--- src/pages/hire-me.js
+++ src/pages/hire-me.js
@@ -9,6 +9,7 @@ import themeSettings from '../components/base/settings';
 
 import Layout from "../components/base/Layout";
 import Header from "../components/base/Header";
+import ContactForm from '../components/hire-me/ContactForm';
 import Footer from "../components/base/Footer";
 
 const Heading = styled.div`
@@ -43,89 +44,7 @@ export default () => (
       <Heading>Hire Connor</Heading>
       <Subheading>Speaking</Subheading>
       <Text>Expenses (airfare and hotel) are extra. If we mutually agree that this is a good fit, you’ll receive a firm quotation on the fee and the expenses. I’ll make my own travel arrangements. The fee and expenses will be due (to be received) two weeks before the actual date.</Text>
-      <form action="https://formspree.io/connorocampo@gmail.com" method="POST">
-        <label type="text" htmlFor="fname-label">First Name</label>
-        <input type="text" htmlFor="name" />
-        <label type="text" htmlFor="lname-label">Last Name</label>
-        <input type="text" htmlFor="name" />
-        <label type="text" htmlFor="email-label">Email</label>
-        <input type="email" htmlFor="_replyto" />
-        <label type="text" htmlFor="state-label">State</label>
-        <select>
-          <option value="AL">Alabama</option>
-          <option value="AK">Alaska</option>
-          <option value="AZ">Arizona</option>
-          <option value="AR">Arkansas</option>
-          <option value="CA">California</option>
-          <option value="CO">Colorado</option>
-          <option value="CT">Connecticut</option>
-          <option value="DE">Delaware</option>
-          <option value="DC">District Of Columbia</option>
-          <option value="FL">Florida</option>
-          <option value="GA">Georgia</option>
-          <option value="HI">Hawaii</option>
-          <option value="ID">Idaho</option>
-          <option value="IL">Illinois</option>
-          <option value="IN">Indiana</option>
-          <option value="IA">Iowa</option>
-          <option value="KS">Kansas</option>
-          <option value="KY">Kentucky</option>
-          <option value="LA">Louisiana</option>
-          <option value="ME">Maine</option>
-          <option value="MD">Maryland</option>
-          <option value="MA">Massachusetts</option>
-          <option value="MI">Michigan</option>
-          <option value="MN">Minnesota</option>
-          <option value="MS">Mississippi</option>
-          <option value="MO">Missouri</option>
-          <option value="MT">Montana</option>
-          <option value="NE">Nebraska</option>
-          <option value="NV">Nevada</option>
-          <option value="NH">New Hampshire</option>
-          <option value="NJ">New Jersey</option>
-          <option value="NM">New Mexico</option>
-          <option value="NY">New York</option>
-          <option value="NC">North Carolina</option>
-          <option value="ND">North Dakota</option>
-          <option value="OH">Ohio</option>
-          <option value="OK">Oklahoma</option>
-          <option value="OR">Oregon</option>
-          <option value="PA">Pennsylvania</option>
-          <option value="RI">Rhode Island</option>
-          <option value="SC">South Carolina</option>
-          <option value="SD">South Dakota</option>
-          <option value="TN">Tennessee</option>
-          <option value="TX">Texas</option>
-          <option value="UT">Utah</option>
-          <option value="VT">Vermont</option>
-          <option value="VA">Virginia</option>
-          <option value="WA">Washington</option>
-          <option value="WV">West Virginia</option>
-          <option value="WI">Wisconsin</option>
-          <option value="WY">Wyoming</option>
-        </select>
-        <label type="text" htmlFor="date-label">Preferred Date</label>
-        <input type="date"></input>
-        <label type="text" htmlFor="speaking-type-label">Speaking Type</label>
-        <select>
-          <option value="AZ">Conference</option>
-          <option value="AL">Internal</option>
-          <option value="AK">Keynote</option>
-          <option value="AR">Local Meetup</option>
-          <option value="CA">Other</option>
-        </select>
-        <label type="text" htmlFor="attendees-label">Number of Attendees</label>
-        <select>
-          <option value="AZ">0-25</option>
-          <option value="AL">25-50</option>
-          <option value="AK">50-100</option>
-          <option value="AR">100-200</option>
-          <option value="CA">200+</option>
-        </select>
-        <label type="text" htmlFor="comments-label">Comments</label>
-        <textarea name="message" placeholder="Test Message"></textarea>
-        <button type="submit" value="Send">Submit</button>
-      </form>
+      <ContactForm/>
       <Footer />
     </Layout>
   </ThemeProvider>

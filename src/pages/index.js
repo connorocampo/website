2a5a9722118f995f3f16b12acb9bd250cae3c928
Connor Ocampo <ConnorOcampo@gmail.com>
2019-10-31 15:53:09 -0400

    Add favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,8 @@ import Book from "../components/home/Book";
 // import Writings from "../components/home/Writings";
 import Footer from "../components/base/Footer";
 
+import favicon from '../../static/favicon.ico';
+
 export default ({ data }) => (
   <ThemeProvider theme={themeSettings}>
     <Layout>
@@ -29,6 +31,7 @@ export default ({ data }) => (
         <meta name="Cat Ocampo's Website" content="Cat Ocampo's Website" />
         <meta name="theme-color" content="#0090D9" />
         <meta name="description" content="The home page for Cat Ocampo's website." />
+        <link rel="icon" href={favicon} />
       </Helmet>
       <Header />
       <Bio />

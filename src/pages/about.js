16e80a78670bbf29b84c748f57c1dc0a4da78c0f
Connor Ocampo <ConnorOcampo@gmail.com>
2019-11-03 07:46:54 -0500

    Add tech

--- src/pages/about.js
+++ src/pages/about.js
@@ -35,14 +35,27 @@ export default () => (
       </Helmet>
       <Header />
       <Heading className="page-title">About Cat Ocampo</Heading>
-      <Text>I’m a native South Floridian who graduated college at UCF in 2016 with a Bachelor of Science Degree. Since then I’ve went on to gain employment in web technologies at a financial publishing company and then later at a web development agency. When I’m not working, I enjoy cooking, painting, playing guitar and singing.
+      <Text>I’m a native South Floridian who graduated college at UCF in 2016 with a Bachelor of Science Degree. Since then I’ve went on to gain employment in web technologies at a financial publishing company and then later at a web development agency. Professionally, I have a history of work in front-end development, search engine optimization (SEO) and a bit of web design.
+      <br />
+        <br />
+        When I’m not working, I enjoy cooking (one pot/pan meals are the best!), painting, playing guitar and singing.
         <br />
         <br />
         I also run a blog about my ongoing experience with <a href="https://www.quittingsocialmedia.com/" target="_blank" rel="noopener noreferrer">quitting social media for one year</a>. As a millennial who has been on social media for nearly half my life, I thought it'd be a good idea to quit social media for a while to see what life is like. So far, so good!
         <br />
         <br />
         Currently, I'm looking for job opportunities utilizing skills I already have and/or learning new things. If you think I'd be a good match to your company mission and culture, <a href="https://catherineocampo.github.io/catherineocampo/contact/" target="_blank" rel="noopener noreferrer">let's chat</a>!
-        </Text>
+        <br />
+        <br />
+        <h2>Tech I've Worked With</h2>
+        <b><u>Front-end:</u></b> Github, Atom, VS Code (my fav), GIT, CSS, SCSS, Tailwind, React, Gatsby, Netlify, Surge.sh. The design aspect is in my wheelhouse in terms of front-end development.
+        <br />
+        <br />
+        <b><u>Design:</u></b> Sketch, Abstract, Photoshop, and good ol' sketch paper.
+        <br />
+        <br />
+        <b><u>Marketing:</u></b> Google Analytics, SEMrush, Hootsuite, Yoast, MailChimp, Buffer. Most of my marketing work consists of monitoring analytics, copywriting headlines and optimizing articles.
+      </Text>
       <Footer />
     </Layout>
   </ThemeProvider>

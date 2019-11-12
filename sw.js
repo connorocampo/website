544a53949a7ff44743ee59e4fa3b17f15274bd9f
Connor Ocampo <ConnorOcampo@gmail.com>
2019-11-11 23:42:16 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6ef9a1d0b6856838fb40.js"
+    "url": "webpack-runtime-7968b4d103d7eef186fe.js"
   },
   {
     "url": "app-3c246e62875cf7917a96.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9e549c3706902d096f202d237815818e"
+    "revision": "8398955656cef4b83c71c96e934a5582"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"

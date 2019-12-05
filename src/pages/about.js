5a0bae54273b92b7d93fb48ed20c6da15707d5a0
Connor Ocampo <connorocampo@gmail.com>
2019-12-04 21:49:29 -0500

    Add design one

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,68 +1,76 @@
-import React from "react";
-import { Helmet } from 'react-helmet';
-import styled from 'styled-components';
-import { ThemeProvider } from "styled-components";
+import React from "react"
+import { Helmet } from "react-helmet"
+import styled from "styled-components"
+import { ThemeProvider } from "styled-components"
 
-import themeSettings from '../components/base/settings';
-import favicon from '../../static/favicon.ico';
+import themeSettings from "../components/base/settings"
+import favicon from "../../static/favicon.ico"
 
-import VeganBeef from "../assets/images/VeganBeef.jpg";
-import Painting from "../assets/images/Painting.jpg";
-
+import VeganBeef from "../assets/images/VeganBeef.jpg"
+import Painting from "../assets/images/Painting.jpg"
 
 // Components
 
-import Layout from "../components/base/Layout";
-import Header from "../components/base/Header";
-import Footer from "../components/base/Footer";
+import Layout from "../components/base/Layout"
+import Header from "../components/base/Header"
+import Footer from "../components/base/Footer"
 
 const Heading = styled.h1`
   font-size: ${props => props.theme.size.headerBig};
   margin: 100px 0 50px 0;
   text-transform: uppercase;
-`;
+`
 
 const CallToAction = styled.p`
-  font-family: 'Open Sans Heavy';
+  font-family: "Open Sans Heavy";
   margin: 0;
-`;
+`
 
 export const Hobbies = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
-`;
+`
 
 export const Image1 = styled.img`
   margin-top: 40px;
   max-width: 310px;
-`;
+`
 
 export const Image2 = styled.img`
   margin-top: 40px;
   max-width: 310px;
-`;
+`
 
 export const LastParagraph = styled.p`
   margin-bottom: 100px;
-`;
+`
 
 export default () => (
   <ThemeProvider theme={themeSettings}>
     <Layout>
       <Helmet>
         <title>About | Connor Ocampo's Website</title>
-        <meta name="About Connor Ocampo's Website" content="About Connor Ocampo's Website" />
+        <meta
+          name="About Connor Ocampo's Website"
+          content="About Connor Ocampo's Website"
+        />
         <meta name="theme-color" content="#0090D9" />
         <link rel="icon" href={favicon} />
       </Helmet>
       <Header />
       <Heading className="page-title">About</Heading>
       <p>
-        I’m a native South Floridian who graduated college at UCF in 2016 with a Bachelor of Science Degree. Since then I’ve went on to gain employment in web technologies at a financial publishing company and then later at a web development agency. Professionally, I have a history of work in front-end development, search engine optimization (SEO), and a bit of web design.
+        I’m a native South Floridian who graduated college at UCF in 2016 with a
+        Bachelor of Science Degree. Since then I’ve went on to gain employment
+        in web technologies at a financial publishing company and then later at
+        a web development agency. Professionally, I have a history of work in
+        front-end development, search engine optimization (SEO), and a bit of
+        web design.
       </p>
       <p>
-        When I’m not working, I enjoy cooking (one pot/pan meals are the best!), painting, playing guitar and singing.
+        When I’m not working, I enjoy cooking (one pot/pan meals are the best!),
+        painting, playing guitar and singing.
       </p>
       <Hobbies>
         <div>
@@ -83,20 +91,45 @@ export default () => (
       </p> */}
       <h2>Note to Hiring Managers</h2>
       <CallToAction>
-        Currently, I'm looking for job opportunities utilizing skills I already have and/or learning new things. If you think I'd be a good match to your company mission and culture, <a href="https://connorocampo.github.io/connorocampo/contact/" target="_blank" rel="noopener noreferrer">let's chat</a>!
+        Currently, I'm looking for job opportunities utilizing skills I already
+        have and/or learning new things. If you think I'd be a good match to
+        your company mission and culture,{" "}
+        <a
+          href="https://connorocampo.github.io/connorocampo/contact/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          let's chat
+        </a>
+        !
       </CallToAction>
       <br />
       <h2>Tech I've Worked With</h2>
       <p>
-        <b><u>Front-end:</u></b> Github, Atom, VS Code (my fav), GIT, Homebrew, CSS, SCSS, Bootstrap, Flexbox, Tailwind, React, styled-components, Gatsby, GraphQL, Netlify, Surge.sh. The design aspect is in my wheelhouse in terms of front-end development.
+        <b>
+          <u>Front-end:</u>
+        </b>{" "}
+        Github, Atom, VS Code (my fav), GIT, Homebrew, CSS, SCSS, Bootstrap,
+        Flexbox, Tailwind, React, styled-components, Gatsby, GraphQL, Netlify,
+        Surge.sh. The design aspect is in my wheelhouse in terms of front-end
+        development.
       </p>
       <p>
-        <b><u>Video/Graphics:</u></b> Sketch, Abstract, Photoshop, good ol' sketch paper, DaVinci Resolve.
+        <b>
+          <u>Video/Design:</u>
+        </b>{" "}
+        Figma, Sketch, Abstract, Photoshop, good ol' sketch paper, DaVinci
+        Resolve.
       </p>
       <LastParagraph>
-        <b><u>Marketing:</u></b> Google Analytics, SEMrush, Hootsuite, Yoast, MailChimp, Buffer. Most of my marketing work consists of monitoring/reporting analytics, copywriting and optimizing articles, and organic social media marketing.
+        <b>
+          <u>Marketing:</u>
+        </b>{" "}
+        Google Analytics, SEMrush, Hootsuite, Yoast, MailChimp, Buffer. Most of
+        my marketing work consists of monitoring/reporting analytics,
+        copywriting and optimizing articles, and organic social media marketing.
       </LastParagraph>
-      <Footer/>
+      <Footer />
     </Layout>
   </ThemeProvider>
 )

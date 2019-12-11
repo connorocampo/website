8ef4e2df6c800eddab5250c231cfb9bec54188c6
Connor Ocampo <connorocampo@gmail.com>
2019-12-10 20:06:26 -0500

    Update header styles

--- src/components/base/Header.js
+++ src/components/base/Header.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from "gatsby";
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
 
 // Styles
 
@@ -9,18 +9,18 @@ export const Container = styled.div`
   align-items: center;
   justify-content: space-between;
   margin-bottom: 100px;
-`;
+`
 
 export const Brand = styled.span`
   font-size: ${props => props.theme.size.headerMedium};
   letter-spacing: 5px;
   line-height: 1;
   text-transform: uppercase;
-`;
+`
 
 export const NavItems = styled.span`
   font-size: ${props => props.theme.size.bodySmall};
-`;
+`
 
 // Component
 
@@ -29,20 +29,29 @@ class Header extends React.Component {
     return (
       <Container className="header-container">
         <Brand className="brand-container">
-          <Link to="/" className="brand">Connor Ocampo</Link>
+          <Link to="/" className="brand">
+            Connor Ocampo
+          </Link>
         </Brand>
         <NavItems className="nav-items navbar-nav">
-          {/* <Link to="/" className="nav-item">Home</Link> */}
-          <Link to="/about/" className="nav-item">About</Link>
-          <Link to="/projects/" className="nav-item">Projects</Link>
-          <Link to="/blog/" className="nav-item">Blog</Link>
-          <Link to="/contact/" className="nav-item">Contact</Link>
+          <Link to="/about/" className="nav-item">
+            About
+          </Link>
+          <Link to="/projects/" className="nav-item">
+            Projects
+          </Link>
+          <Link to="/blog/" className="nav-item">
+            Blog
+          </Link>
+          <Link to="/contact/" className="nav-item">
+            Contact
+          </Link>
           {/* <Link to="/shop/" className="nav-item">Shop</Link> */}
           {/* <Link to="/hire-me/" className=" nav-item">Hire Me</Link> */}
         </NavItems>
       </Container>
-    );
+    )
   }
 }
 
-export default Header;
+export default Header

a72b90b2914053768aeae8df31664910f013af6a
Connor Ocampo <connorocampo@gmail.com>
2019-12-29 22:32:50 -0500

    Remove overflow from layout

--- src/components/base/Layout.js
+++ src/components/base/Layout.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default ({ children }) => (
-  <div style={{ height: "100%", margin: "0", padding: "0", overflow-x: "hidden" }}>{children}</div>
+  <div style={{ height: "100%", margin: "0", padding: "0" }}>{children}</div>
 )

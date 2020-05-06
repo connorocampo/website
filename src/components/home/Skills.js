feaa941f38f317f24596f1a4809f69a20a16dd36
Connor Ocampo <connorocampo@gmail.com>
2020-05-06 18:39:51 -0400

    Update skills

--- src/components/home/Skills.js
+++ src/components/home/Skills.js
@@ -65,29 +65,7 @@ class Skills extends React.Component {
         <Container>
           <Columns className="has-box-shadow skills-columns">
             <div className="column column-1">
-              <Icon src={Design} alt="design icon" />
-              <ColumnHeader>Video/Design</ColumnHeader>
-              <ColumnText style={{ marginBottom: "30px" }}>
-                I enjoy learning, designing, and experimenting with simple,
-                modern, and intuitive design for desktop and mobile devices.
-              </ColumnText>
-              <ColumnText style={{ marginBottom: "75px" }}>
-                I also enjoy learning more about video production software to
-                create impactful films.
-              </ColumnText>
-              <ColumnHeader>Video/Design Tools</ColumnHeader>
-              <List style={{ marginBottom: "30px" }}>
-                <ListItem>DaVinci Resolve</ListItem>
-                <ListItem>Figma</ListItem>
-                <ListItem>Sketch</ListItem>
-                <ListItem>Abstract</ListItem>
-                <ListItem>Photoshop</ListItem>
-                <ListItem>Paper & Pencil</ListItem>
-              </List>
-            </div>
-            <hr className="hr-border" />
-            <div className="column column-2">
-              <Icon src={Dev} alt="development icon" />
+            <Icon src={Dev} alt="development icon" />
               <ColumnHeader>Front-end Development</ColumnHeader>
               <ColumnText style={{ marginBottom: "30px" }}>
                 <span className="regular-fw">I am proficient</span> in HTML,
@@ -108,8 +86,9 @@ class Skills extends React.Component {
                 <ListItem>Heroku</ListItem>
               </List>
             </div>
-            <div className="column column-3">
-              <Icon src={Marketing} alt="marketing/seo icon" />
+            <hr className="hr-border" />
+            <div className="column column-2">
+            <Icon src={Marketing} alt="marketing/seo icon" />
               <ColumnHeader>Marketing/SEO</ColumnHeader>
               <ColumnText style={{ marginBottom: "30px" }}>
                 Most of my marketing/seo work consists of optimizing articles,
@@ -127,6 +106,29 @@ class Skills extends React.Component {
                 <ListItem>SEMrush</ListItem>
                 <ListItem>Hootsuite</ListItem>
                 <ListItem>MailChimp</ListItem>
+              </List>
+            </div>
+            <div className="column column-3">
+
+              <Icon src={Design} alt="design icon" />
+              
+              <ColumnHeader>Video/Design</ColumnHeader>
+              <ColumnText style={{ marginBottom: "30px" }}>
+                I enjoy learning, designing, and experimenting with simple,
+                modern, and intuitive design for desktop and mobile devices.
+              </ColumnText>
+              <ColumnText style={{ marginBottom: "75px" }}>
+                I also enjoy learning more about video production software to
+                create impactful films.
+              </ColumnText>
+              <ColumnHeader>Video/Design Tools</ColumnHeader>
+              <List style={{ marginBottom: "30px" }}>
+                <ListItem>DaVinci Resolve</ListItem>
+                <ListItem>Figma</ListItem>
+                <ListItem>Sketch</ListItem>
+                <ListItem>Abstract</ListItem>
+                <ListItem>Photoshop</ListItem>
+                <ListItem>Paper & Pencil</ListItem>
               </List>
             </div>
           </Columns>
